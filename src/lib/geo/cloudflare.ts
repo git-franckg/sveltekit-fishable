156d8f20f36781e8127c6740d260a40f5d873068
Franck GOGO <franck.gogo@outlook.com>
2025-05-21 19:12:41 +0200

    use cf properties in request

--- src/lib/geo/cloudflare.ts
+++ src/lib/geo/cloudflare.ts
@@ -1,27 +1,35 @@
 import type { Fishable } from '../index.js';
 import type { GeoProvider } from './index.js';
+import type { IncomingRequestCfPropertiesGeographicInformation } from '@cloudflare/workers-types';
 import { getByIso2 } from '@git-franckg/countries';
 import type { CountryIso2 } from '@git-franckg/countries';
 
+type CfRequest = Request & { cf: IncomingRequestCfPropertiesGeographicInformation };
+
 export const Cloudflare: GeoProvider = {
-  available(_building, event) {
-    return event.request.headers.has('cf-connecting-ip');
+  available(_building, { request }) {
+    const cfReq = request as CfRequest;
+    return Boolean(cfReq.cf) && typeof cfReq.cf === 'object';
   },
 
   async provide(event) {
-    const { headers } = event.request;
+    const { cf } = event.request as CfRequest;
 
-    const ip = headers.get('cf-connecting-ip'),
-      countryIso2 = headers.get('cf-ipcountry'),
-      postalCode = headers.get('cf-postal-code'),
-      city = headers.get('cf-ipcity');
+    const ip = event.getClientAddress(),
+      countryIso2 = cf.country,
+      postalCode = cf.postalCode,
+      city = cf.city;
+    // const ip = headers.get('cf-connecting-ip'),
+    //   countryIso2 = headers.get('cf-ipcountry'),
+    //   postalCode = headers.get('cf-postal-code'),
+    //   city = headers.get('cf-ipcity');
 
     if (!ip) throw new Error('Missing CF-Connecting-IP');
     else if (!countryIso2) throw new Error('Cloudflare "IP geolocation" is required.');
-    else if (!postalCode || !city) throw new Error('Cloudflare "Add visitor location headers" is required.');
+    else if (!postalCode || !city) throw new Error('Cloudflare "Add visitor location headers" is required. Client IP: ' + ip);
 
     if (countryIso2 == 'T1') throw new Error('Client IP is Tor.');
-    else if (countryIso2 == 'XX') throw new Error('Client IP country is unknown.');
+    else if (!countryIso2) throw new Error('Client IP country is unknown.');
 
     return {
       ip,
